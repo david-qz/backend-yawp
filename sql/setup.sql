92b840bf5310eeba0eab4e56483368206be65a45
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-16 12:39:38 -0700

    Move seed restaurant data into main sql file
    I guess I want this data to exist on the deployed version too.

--- sql/setup.sql
+++ sql/setup.sql
@@ -20,6 +20,11 @@ create table restaurants(
     image text
 );
 
+insert into restaurants (name, cuisine, cost, website, image) values
+('Andina', 'Peruvian', 3, 'https://www.andinarestaurant.com/', 'https://images.squarespace-cdn.com/content/v1/5b747f617e3c3a0811711c60/8e16f160-e74f-4112-8e34-a504af331ff9/Dina+Avila_Andina_5210.jpg?format=2500w'),
+('Top Burmese', 'Burmese', 2, 'https://topburmese.com/', 'https://cdn.shopify.com/s/files/1/0063/5040/5732/files/Top_Burmese_Samosas_Best_Burmese_Food_fea2db54-55f6-4651-8910-a588a4470313_720x.JPEG?v=1550123611'),
+('Syun Izakaya', 'Japanese', 2, 'https://www.syunhillsboro.com/', 'https://www.syunhillsboro.com/uploads/1/3/4/6/134642574/s399356810435617057_p228_i1_w1000.jpeg?width=1200');
+
 create table reviews(
     id bigint generated always as identity primary key,
     user_id bigint references users (id),
