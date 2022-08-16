6c81b290ed2c4699e15193eaae4a465c6ae03f28
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-15 17:13:57 -0700

    Implement POST /api/v1/sessions
    Logs the user in.

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -33,6 +33,33 @@ describe('/api/v1/users routes', () => {
         expect(response2.status).toEqual(409);
     });
 
+    it('#POST /api/v1/users/sessions should log a user in', async () => {
+        const agent = request.agent(app);
+        // Make a user
+        await agent.post('/api/v1/users').send({ email: 'alice@test.com', password: '123456' });
+        // Log out
+        await agent.delete('/api/v1/users/sessions');
+
+        // Log back in
+        const response = await agent.post('/api/v1/users/sessions').send({ email: 'alice@test.com', password: '123456' });
+        expect(response.status).toEqual(200);
+
+        const session = agent.jar.getCookie(process.env.COOKIE_NAME, CookieAccessInfo.All);
+        expect(session).toBeTruthy();
+    });
+
+    it('#POST /api/v1/users/sessions should error on bad credentials', async () => {
+        const agent = request.agent(app);
+        // Make a user
+        await agent.post('/api/v1/users').send({ email: 'alice@test.com', password: '123456' });
+        // Log out
+        await agent.delete('/api/v1/users/sessions');
+
+        // Log back in
+        const response = await agent.post('/api/v1/users/sessions').send({ email: 'alice@test.com', password: 'qwerty' });
+        expect(response.status).toEqual(401);
+    });
+
     it('#DELETE /api/v1/users/sessions should log a user out', async () => {
         const agent = request.agent(app);
 
