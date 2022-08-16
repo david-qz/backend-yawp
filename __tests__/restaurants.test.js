44301e1d7ba14e7bb022ff63b05bc5a26b3f0e6c
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-16 10:28:34 -0700

    Implement GET /api/v1/restaurants/:id

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -27,6 +27,29 @@ describe('/api/v1/restaurants routes', () => {
         }));
     });
 
+    it('#GET /api/v1/restaurants/:id should return restaurant with id and nested reviews', async () => {
+        const response = await request(app).get('/api/v1/restaurants/1');
+        expect(response.status).toEqual(200);
+
+        const restaurant = response.body;
+        expect(restaurant).toEqual({
+            id: expect.any(String),
+            name: expect.any(String),
+            cuisine: expect.any(String),
+            cost: expect.any(Number),
+            website: expect.any(String),
+            image: expect.any(String),
+            reviews: expect.any(Array)
+        });
+        restaurant.reviews.forEach(review => expect(review).toEqual({
+            id: expect.any(Number),
+            restaurantId: expect.any(Number),
+            userId: expect.any(Number),
+            detail: expect.any(String),
+            stars: expect.any(Number)
+        }));
+    });
+
     afterAll(async () => {
         await setup(pool);
         pool.end();
